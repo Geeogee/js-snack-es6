4d03f6a4b0d2d1f6b35fed22435f5b4d4dde99f8
Gioele <gioele.armiento@gmail.com>
2021-03-26 15:27:05 +0100

    Exercise 2 (part 2/3) added

--- js/script.js
+++ js/script.js
@@ -47,9 +47,73 @@ function exercise1() {
 
 }
 
+// Creare un array di oggetti di squadre di calcio. 
+// Ogni squadra avrà diverse proprietà: 
+// nome, punti fatti, falli subiti.
+// Nome sarà l'unica proprietà da compilare, 
+// le altre saranno tutte settate a 0.
+
+function genRandomValue(min,max) {
+
+    var rndMin = min;
+    var rndMax = max - min + 1;
+
+    var rndNum = Math.floor(Math.random() * rndMax) + rndMin;
+
+    return rndNum;
+}
+
+function exercise2() {
+
+    const TEAMS = [
+        {
+            "name" : "U.S. Matinum",
+            "points" : 0,
+            "fouls" : 0
+        },
+
+        {
+            "name" : "Selecao",
+            "points" : 0,
+            "fouls" : 0
+        },
+
+        {
+            "name" : "U.S. Foggia",
+            "points" : 0,
+            "fouls" : 0
+        },
+
+        {
+            "name" : "Donia Calcio",
+            "points" : 0,
+            "fouls" : 0
+        },
+
+        {
+            "name" : "Atletico Vieste",
+            "points" : 0,
+            "fouls" : 0
+        },  
+    ];
+
+    // Generare numeri random al posto degli 0 nelle proprietà:
+    // punti fatti e falli subiti
+    
+    for (let i=0; i<TEAMS.length; i++) {
+
+        var TEAM = TEAMS[i];
+        TEAM["points"] = genRandomValue(0,100);
+        TEAM["fouls"] = genRandomValue(0,10);
+    }
+
+    console.log(TEAMS);
+}
+
 function init() {
 
-    exercise1();
+    // exercise1();
+    exercise2();
 }
 
 document.addEventListener("DOMContentLoaded", init());
